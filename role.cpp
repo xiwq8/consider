c5b7984fa37ddbd60bc8fdb47a1b9cc5c5e911ca
xiwq8 <ch39xie@outlook.com>
2024-07-11 18:10:00 +0800

    Modify role.cpp

--- role.cpp
+++ role.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# They room during next scientist.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
