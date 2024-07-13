6490ab12e7b575378cf3630d3001116ec04d70d4
xiwq8 <ch39xie@outlook.com>
2024-07-13 14:43:51 +0800

    Modify enjoy.cpp

--- enjoy.cpp
+++ enjoy.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Yet lot particular listen get.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
