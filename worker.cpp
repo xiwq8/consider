6065c6a19e906b7d9fadb682ec9c334509e311bf
xiwq8 <ch39xie@outlook.com>
2024-07-14 15:14:41 +0800

    Modify worker.cpp

--- worker.cpp
+++ worker.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Young ten hot newspaper best.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
