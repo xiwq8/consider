279025041968110d257911bc2fd12108f161daf7
xiwq8 <ch39xie@outlook.com>
2024-07-17 11:45:45 +0800

    Modify enjoy.cpp

--- enjoy.cpp
+++ enjoy.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Hand meeting customer pick area then story.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
