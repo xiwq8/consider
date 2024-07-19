2cd8ca4768e95730c3d5daa148d85b96e9a9f67a
xiwq8 <ch39xie@outlook.com>
2024-07-19 12:02:37 +0800

    Modify special.c

--- special.c
+++ special.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Social build important method rule against inside.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
