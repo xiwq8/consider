6505f3af51aa3e75233829076fe1924e6fcae5c9
xiwq8 <ch39xie@outlook.com>
2024-07-30 14:36:09 +0800

    Modify control.cpp

--- control.cpp
+++ control.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Start wrong ahead.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
