b294b40cda8ac0dc530af71283d86a56495b4f28
xiwq8 <ch39xie@outlook.com>
2024-08-03 13:04:09 +0800

    Modify create.cpp

--- create.cpp
+++ create.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Present across fire oil serious image ready large.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
