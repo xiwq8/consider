6c9b3dddbed3611c6cdd39b30e1fe2b4eeacb682
xiwq8 <ch39xie@outlook.com>
2024-08-16 08:13:04 +0800

    Modify enjoy.cpp

--- enjoy.cpp
+++ enjoy.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Build him good.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
