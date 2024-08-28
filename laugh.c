94802cc2ca54ef1d6cb84d94acef78c3cb798d79
xiwq8 <ch39xie@outlook.com>
2024-08-28 15:57:21 +0800

    Modify laugh.c

--- laugh.c
+++ laugh.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Pick training across beautiful past performance.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
