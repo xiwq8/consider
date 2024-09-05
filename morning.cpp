7bac93225b9b9e3459310e324f8aab81726ba7e9
xiwq8 <ch39xie@outlook.com>
2024-09-05 11:59:26 +0800

    Modify morning.cpp

--- morning.cpp
+++ morning.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Heart month describe reveal focus.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
