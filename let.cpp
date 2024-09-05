d919affd06c37b0fbda2203e105d5e8d0153c7e6
xiwq8 <ch39xie@outlook.com>
2024-09-05 11:59:31 +0800

    Modify let.cpp

--- let.cpp
+++ let.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# City two less news manager would number become.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
