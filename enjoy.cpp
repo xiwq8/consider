365de7f1832f38f9a197f16375f0d7ae1c1c6034
xiwq8 <ch39xie@outlook.com>
2024-09-20 19:44:13 +0800

    Modify enjoy.cpp

--- enjoy.cpp
+++ enjoy.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# We machine positive.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
