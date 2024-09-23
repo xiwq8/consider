4fda9beee544fcf0d02eee36fbb292727be1a9ef
xiwq8 <ch39xie@outlook.com>
2024-09-23 12:03:55 +0800

    Modify forward.cpp

--- forward.cpp
+++ forward.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Help college bill.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
