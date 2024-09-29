01463644d6c6c45e81df747586da282ad6b418de
xiwq8 <ch39xie@outlook.com>
2024-09-29 13:36:04 +0800

    Modify forward.cpp

--- forward.cpp
+++ forward.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Expect ask set cell woman alone.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
