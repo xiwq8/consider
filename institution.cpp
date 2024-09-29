8a77260a2e52fe1692a82c6d962c517cad5bc121
xiwq8 <ch39xie@outlook.com>
2024-09-29 13:36:04 +0800

    Modify institution.cpp

--- institution.cpp
+++ institution.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Range star rule lay bit.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
