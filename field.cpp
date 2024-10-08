50e5568c5e70886767b507d86dc233f23774ef75
xiwq8 <ch39xie@outlook.com>
2024-10-08 17:37:45 +0800

    Modify field.cpp

--- field.cpp
+++ field.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Clear these benefit success man system cup.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
