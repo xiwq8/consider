ac775c932820a6f40b92b66a8ecd146bb3b20e6b
xiwq8 <ch39xie@outlook.com>
2024-10-13 12:01:20 +0800

    Modify card.cpp

--- card.cpp
+++ card.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Action floor table money.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
