2570ce3fb168a6a7629f61082c1ac7194cbcd1f6
xiwq8 <ch39xie@outlook.com>
2024-10-15 11:00:57 +0800

    Modify morning.cpp

--- morning.cpp
+++ morning.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Pay receive if since rock long that.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
