5014fcb4d3372b788d2b2838b55fcb564255dbf2
xiwq8 <ch39xie@outlook.com>
2024-10-17 10:47:38 +0800

    Modify management.cpp

--- management.cpp
+++ management.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Style part each series pay.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
