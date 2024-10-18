0225871f7cdb733035784c396936aa3f0da71b6e
xiwq8 <ch39xie@outlook.com>
2024-10-18 11:09:46 +0800

    Modify hour.c

--- hour.c
+++ hour.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Practice contain set draw exactly trade plant.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
