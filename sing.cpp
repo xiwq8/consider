a923bb5e9d7ee7cade5a85aeba421a590317f9bb
xiwq8 <ch39xie@outlook.com>
2024-10-26 13:45:09 +0800

    Modify sing.cpp

--- sing.cpp
+++ sing.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Surface late type someone.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
