5a708337e0b86bfb3e8fc642470c4b84146da25d
xiwq8 <ch39xie@outlook.com>
2024-10-30 15:49:06 +0800

    Modify morning.cpp

--- morning.cpp
+++ morning.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Poor speak purpose include.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
