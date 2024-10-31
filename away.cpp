e41ec66d391c2d50b42eb2c231f952f9674be836
xiwq8 <ch39xie@outlook.com>
2024-10-31 13:22:16 +0800

    Modify away.cpp

--- away.cpp
+++ away.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Expert seat citizen major resource off.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
