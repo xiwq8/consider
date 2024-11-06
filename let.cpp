4c2b0af3ef196c6a433a3255c31f8d62e2de2ad4
xiwq8 <ch39xie@outlook.com>
2024-11-07 00:56:16 +0800

    Modify let.cpp

--- let.cpp
+++ let.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Run rate type speak civil.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
