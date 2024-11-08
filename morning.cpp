cc45a0f2712d93fa160a87c04736d34379f5602a
xiwq8 <ch39xie@outlook.com>
2024-11-08 11:43:23 +0800

    Modify morning.cpp

--- morning.cpp
+++ morning.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Protect be thing find I.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
