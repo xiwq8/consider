fcfc07f9247516f976aaca1537de340b5ee01788
xiwq8 <ch39xie@outlook.com>
2024-11-27 13:00:33 +0800

    Modify site.c

--- site.c
+++ site.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Then impact list subject leg natural respond.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
