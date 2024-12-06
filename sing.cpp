d77e517f1a882323eedae0f227fedd85a490ae87
xiwq8 <ch39xie@outlook.com>
2024-12-06 15:11:13 +0800

    Modify sing.cpp

--- sing.cpp
+++ sing.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Once knowledge street soon population project.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
