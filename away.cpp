0b744e8c9dd1f54a2edcb55197d5901fd1df2e76
xiwq8 <ch39xie@outlook.com>
2025-01-03 16:42:23 +0800

    Modify away.cpp

--- away.cpp
+++ away.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Full sit manage partner.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
