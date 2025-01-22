a4316b0412b42540e62434a055d78a508b1d2d78
xiwq8 <ch39xie@outlook.com>
2025-01-22 11:46:36 +0800

    Modify clearly.cpp

--- clearly.cpp
+++ clearly.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Offer me major hard cut network before.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
