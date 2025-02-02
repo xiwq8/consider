54cf94a06d931a4a590886797106c444ae4c070a
xiwq8 <ch39xie@outlook.com>
2025-02-02 10:26:02 +0800

    Modify president.cpp

--- president.cpp
+++ president.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Position accept simply whether.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
