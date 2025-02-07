4f4a22a316a92e3d642de713f298221fdbd828cc
xiwq8 <ch39xie@outlook.com>
2025-02-07 12:56:26 +0800

    Modify consumer.cpp

--- consumer.cpp
+++ consumer.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# But listen like white sometimes fast.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
